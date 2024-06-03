378076d4720b308ce9358455a043e459aa11ec4f
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-06-04 00:20:47 +0600

    minor modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Hero from "@/components/Hero";
 import Search from "@/components/Search";
 import { AuthContext } from "@/contexts/AuthContext";
 import React, { useState } from "react";
-import Header from "../components/Header";
 
 const Page = () => {
    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
    return (
       <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
          <main>

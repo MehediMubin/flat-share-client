121f6d0115b646587a4e6dbe4f84ad8610aa8bc6
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-06-05 01:34:37 +0600

    footer implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
             <Toaster position="bottom-right" />
             <Header />
             {children}
+            <Footer />
          </body>
       </html>
    );

ea9993c018ed6191b0fbd3d683f5c4791359f8a2
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-06-05 13:25:05 +0600

    minor modification

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -56,7 +56,7 @@ const Page = () => {
 
    return (
       <div>
-         <div className="h-screen-16 flex items-center justify-center bg-gray-50 py-1 px-4 sm:px-6 lg:px-8">
+         <div className="h-screen-16 flex items-center justify-center bg-gray-50 py-16 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8 shadow-lg p-6 rounded-lg bg-white">
                <div>
                   <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">

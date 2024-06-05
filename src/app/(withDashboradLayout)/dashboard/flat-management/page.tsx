05bf35128194ac4b4051ab07b0b2cdf72a987919
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2024-06-05 15:15:01 +0600

    flat management modified

--- src/app/(withDashboradLayout)/dashboard/flat-management/page.tsx
+++ src/app/(withDashboradLayout)/dashboard/flat-management/page.tsx
@@ -1,11 +1,56 @@
-import React from "react";
+"use client";
+import MyFlatCard from "@/components/MyFlatCard";
+import React, { useEffect, useState } from "react";
 
-const page = () => {
+export interface Flat {
+   _id: string;
+   location: string;
+   description: string;
+   rent: number;
+   numberOfBedrooms: number;
+   amenities: string;
+   photoUrl: string;
+}
+
+const Page = () => {
+   const [flats, setFlats] = useState([]);
+
+   useEffect(() => {
+      fetch(`http://localhost:5000/api/flats`)
+         .then((response) => response.json())
+         .then((data) => setFlats(data.data))
+         .catch((error) => console.error("Error:", error));
+   }, []);
+
+   const handleDelete = async (flatId: string) => {
+      const token = localStorage.getItem("token");
+      await fetch(`http://localhost:5000/api/flats/${flatId}`, {
+         method: "DELETE",
+         headers: {
+            Authorization: `${token}`,
+         },
+      });
+      setFlats(flats.filter((flat: Flat) => flat._id !== flatId));
+   };
+
    return (
-      <div>
-         <h1>Flat Management Page</h1>
+      <div className="px-2 md:px-8 lg:px-16 py-4">
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 justify-center">
+            {flats.map((flat: Flat, index: number) => (
+               <MyFlatCard
+                  key={index}
+                  flatId={flat._id}
+                  location={flat.location}
+                  description={flat.description}
+                  rent={flat.rent}
+                  amenities={flat.amenities}
+                  photoUrl={flat.photoUrl}
+                  onDelete={handleDelete}
+               />
+            ))}
+         </div>
       </div>
    );
 };
 
-export default page;
+export default Page;
--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -32,11 +32,58 @@ const DashboardHeader = () => {
    return (
       <div className="navbar bg-base-100">
          <div className="navbar-start">
+            <div className="dropdown">
+               <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost lg:hidden"
+               >
+                  <svg
+                     xmlns="http://www.w3.org/2000/svg"
+                     className="h-5 w-5"
+                     fill="none"
+                     viewBox="0 0 24 24"
+                     stroke="currentColor"
+                  >
+                     <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h8m-8 6h16"
+                     />
+                  </svg>
+               </div>
+               <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+               >
+                  <li>
+                     <Link href="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                     <Link href="/dashboard/flat-management">
+                        Flat Management
+                     </Link>
+                  </li>
+                  <li>
+                     <Link href="/dashboard/user-management">
+                        User Management
+                     </Link>
+                  </li>
+                  <li>
+                     {isAuthenticated ? (
+                        <button onClick={handleLogout}>Logout</button>
+                     ) : (
+                        <Link href="/login">Login</Link>
+                     )}
+                  </li>
+               </ul>
+            </div>
             <Link className="btn btn-ghost text-xl" href="/dashboard">
                Dashboard
             </Link>
          </div>
-         <div className="navbar-end">
+         <div className="navbar-end hidden lg:flex">
             <div className="flex items-stretch">
                <ul className="menu menu-horizontal px-1">
                   <li>
